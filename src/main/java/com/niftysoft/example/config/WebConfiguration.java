be2a09c0f9723ab80b23ab252f97c56e75c827db
K. Alex Mills <k.alex.mills@gmail.com>
2018-11-24 17:49:02 -0600

    fix: remove unused aop nonsense.

--- src/main/java/com/niftysoft/example/config/WebConfiguration.java
+++ src/main/java/com/niftysoft/example/config/WebConfiguration.java
@@ -11,23 +11,6 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
 @EnableSpringDataWebSupport
-@EnableAspectJAutoProxy
 public class WebConfiguration {
 
-    public @Bean CustomizableTraceInterceptor interceptor() {
-
-        CustomizableTraceInterceptor interceptor = new CustomizableTraceInterceptor();
-        interceptor.setEnterMessage("Entering $[methodName]($[arguments]).");
-        interceptor.setExitMessage("Leaving $[methodName](..) with return value $[returnValue], took $[invocationTime]ms.");
-
-        return interceptor;
-    }
-
-    public @Bean Advisor traceAdvisor() {
-
-        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
-        pointcut.setExpression("execution(public * org.springframework.data.repository.Repository+.*(..))");
-
-        return new DefaultPointcutAdvisor(pointcut, interceptor());
-    }
 }
