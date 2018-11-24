4f226022d2884e37931fe521fa2e0252c5f1848d
K. Alex Mills <k.alex.mills@gmail.com>
2018-11-24 17:46:58 -0600

    fix: remove target directory.

--- src/main/java/com/niftysoft/example/config/WebConfiguration.java
+++ src/main/java/com/niftysoft/example/config/WebConfiguration.java
@@ -1,16 +1,33 @@
 package com.niftysoft.example.config;
 
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.interceptor.CustomizableTraceInterceptor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.data.repository.support.DomainClassConverter;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import javax.inject.Inject;
 
 @Configuration
 @EnableSpringDataWebSupport
+@EnableAspectJAutoProxy
 public class WebConfiguration {
 
+    public @Bean CustomizableTraceInterceptor interceptor() {
+
+        CustomizableTraceInterceptor interceptor = new CustomizableTraceInterceptor();
+        interceptor.setEnterMessage("Entering $[methodName]($[arguments]).");
+        interceptor.setExitMessage("Leaving $[methodName](..) with return value $[returnValue], took $[invocationTime]ms.");
+
+        return interceptor;
+    }
+
+    public @Bean Advisor traceAdvisor() {
+
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(public * org.springframework.data.repository.Repository+.*(..))");
+
+        return new DefaultPointcutAdvisor(pointcut, interceptor());
+    }
 }
