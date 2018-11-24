b16867813d2eecfc37b51e6cf704fcda4ddb34f4
K. Alex Mills <k.alex.mills@gmail.com>
2018-11-24 10:59:53 -0600

    refactor: create WidgetRepositorySprocketsOnlyTest.java

--- src/test/java/com/niftysoft/example/repository/WidgetRespositoryTest.java
+++ src/test/java/com/niftysoft/example/repository/WidgetRespositoryTest.java
@@ -1,17 +1,12 @@
 package com.niftysoft.example.repository;
 
-import com.niftysoft.example.model.Sprocket;
 import com.niftysoft.example.model.Widget;
-import com.niftysoft.example.model.views.WidgetViews;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.inject.Inject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,8 +16,6 @@ public class WidgetRespositoryTest {
     @Inject
     WidgetRepository underTest;
 
-    @Inject
-    WidgetRepository.SprocketsOnly underTestSprocketsOnly;
 
     @Test
     public void testSave() {
@@ -33,25 +26,6 @@ public class WidgetRespositoryTest {
         assertThat(w.getId()).isNotNull();
         assertThat(underTest.existsById(w.getId())).isTrue();
         assertThat(underTest.findById(w.getId())).contains(w);
-    }
-
-    @Test
-    public void testCascadingSaveWithSprocketsView() {
-        Widget w = new Widget(Widget.Type.ALPHA);
-
-        List<Sprocket> sprockets = new ArrayList<>();
-
-        sprockets.add(new Sprocket(Sprocket.Type.ALEPH));
-        sprockets.add(new Sprocket(Sprocket.Type.BET));
-
-        w.setSprockets(sprockets);
-
-        // This save should cascade, even though Sprockets were not saved individually.
-        underTest.save(w);
-
-        WidgetViews.SprocketsOnly sprocketsFound = underTestSprocketsOnly.findById(w.getId());
-
-        assertThat(sprocketsFound.getSprockets()).containsExactlyElementsOf(sprockets);
     }
 
 }
