9601c60fb2cef2a4e27f3ed6b4117eb661bc7cf5
K. Alex Mills <k.alex.mills@gmail.com>
2018-11-23 18:18:40 -0600

    doc: add WidgetViews documentation

--- src/main/java/com/niftysoft/example/model/views/WidgetViews.java
+++ src/main/java/com/niftysoft/example/model/views/WidgetViews.java
@@ -4,6 +4,10 @@ import com.niftysoft.example.model.Sprocket;
 
 import java.util.List;
 
+/**
+ * WidgetViews provides a common namespace for interfaces used as separate views of a Widget. Spring Data JPA *should*
+ * only query the data required to "fill out" the interface (otherwise, why is this a feature?)
+ */
 public abstract class WidgetViews {
     /**
      * IdOnly is a view interface.
